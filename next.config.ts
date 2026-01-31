df260cc9dc627e5b0155c397a2cd75bb9fa1ba9f
sSyRuSs <89365131+sSyRuSs@users.noreply.github.com>
2026-01-31 09:52:08 +0700

    fix: set BASE_PATH for GitHub Pages deployment in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.BASE_PATH || "",
+  basePath: process.env.BASE_PATH || "/portfolio-web",
   images: {
     unoptimized: true,
   },
